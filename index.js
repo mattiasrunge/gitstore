2e524ecba198050799427b260765c50d85af4cd7
Mattias Runge <mattias@runge.se>
2015-09-27 10:41:56 +0200

    Converted to using Koa and generators

--- index.js
+++ index.js
@@ -1,86 +1,96 @@
 "use strict";
 
-let fs = require("fs");
-let exec = require("child_process").exec
-let express = require("express");
-let app = express();
-let bodyParser = require("body-parser");
+let fs = require("fs-promise");
+let exec = require("child-process-promise").exec;
+let koa = require("koa");
+let bodyParser = require("koa-bodyparser");
+let route = require("koa-route");
 let scriptDir = __dirname + "/scripts/";
 
 let config = require("/etc/gitstore.json");
 
-app.use(bodyParser.json());
+let KoaError = function(message, status) {
+    this.name = "KoaError";
+    this.message = message;
+    this.status = status || 500;
+    this.stack = (new Error()).stack;
+    this.toString = () => this.name + ": (status " + this.status + ") " + this.message;
+};
+KoaError.prototype = new Error();
+KoaError.prototype.name = "KoaError";
 
-app.get("/", (req, res) => {
-    fs.readdir(config.repoDir, (error, items) => {
-        if (error) {
-            console.error("Could not list repositories, error: " + error);
-            return res.status(500).send("Could not list repositories, error: " + error);
-        }
+let app = koa();
+app.use(bodyParser());
 
-        res.type("text").send(items.map(item => "ssh://" + config.hostname + config.repoDir + "/" + item).join("\n"));
-    });
-});
-
-app.post("/github", (req, res) => {
-    if (req.headers["x-github-event"] === "push") {
-        fs.exists(config.repoDir + "/" + req.body.repository.name, exists => {
-            if (!exists) {
-                console.error("Got GitHub push for repository we do not recognize, " + req.body.repository.name);
-                res.status(404).end();
-                return;
-            }
-
-            console.log("Repository " + req.body.repository.name + " updated at GitHub!");
-            let cmd = scriptDir + "update.sh " + config.repoDir + "/" + req.body.repository.name + " " + req.body.repository.ssh_url;
-
-            console.log("Executing " + cmd);
-            exec(cmd, (error, stdout, stderr) => {
-                if (error) {
-                    console.error("Failed to execute command, error: " + error);
-                    return res.status(500).end();
-                }
-
-                console.log(stdout);
-                console.log(stderr);
-                res.end();
-            });
-        });
-    } else if (req.headers["x-github-event"] === "ping") {
-        console.error("Got GitHub ping request");
-        res.end();
-    } else {
-        console.error("Got unknown GitHub request");
-        res.status(400).end();
-        return;
+app.use(function*(next) {
+    try {
+        yield next;
+    } catch (error) {
+        console.error(error);
+        this.response.status = error.status || 500;
+        this.type = "text/plain";
+        this.body = error.message || error;
     }
 });
 
-app.get("/update/:name", (req, res) => {
-    console.log("Repository " + req.params.name + " updated!");
-    res.end();
-});
+app.use(route.get("/", function*(next) {
+    let items = yield fs.readdir(config.repoDir);
+    this.type = "text/plain";
+    this.body = items.map(item => "ssh://" + config.hostname + config.repoDir + "/" + item).join("\n");
+}));
 
-app.get("/create/:name", (req, res) => {
+app.use(route.get("/github", function*(next) {
+    if (req.headers["x-github-event"] !== "push" & req.headers["x-github-event"] !== "ping") {
+        throw new KoaError("Got unknown GitHub request: " + req.headers["x-github-event"], 400);
+    }
+
+    let exists = yield fs.exists(config.repoDir + "/" + req.body.repository.name);
+
+    if (!exists) {
+        throw KoaError("Got GitHub push for repository we do not recognize, " + req.body.repository.name, 404);
+    }
+
+    console.log("Repository " + req.body.repository.name + " updated at GitHub!");
+    let cmd = scriptDir + "update.sh " + config.repoDir + "/" + req.body.repository.name + " " + req.body.repository.ssh_url;
+
+    console.log("Executing " + cmd);
+    let result = yield exec(cmd);
+
+    console.log(result.stdout);
+    console.log(result.stderr);
+    console.log("Repository " + req.body.repository.name + " updated!");
+    this.type = "text/plain";
+    this.body = "OK";
+}));
+
+app.use(route.get("/update/:name", function*(next) {
+    console.log("Repository " + req.params.name + " updated!");
+    this.type = "text/plain";
+    this.body = "OK";
+}));
+
+app.use(route.get("/create/:name", function*(req, res) {
+    let exists = yield fs.exists(config.repoDir + "/" + req.params.name);
+
+    if (exists) {
+        throw KoaError("Repository already exists, " + req.body.repository.name, 409);
+    }
+
     let cmd = scriptDir + "create.sh " + scriptDir + " " + config.repoDir + " " + req.params.name + " http://" + config.hostname;
     let repo = "ssh://" + config.hostname + config.repoDir + "/" + req.params.name;
 
     console.log("Executing " + cmd);
-    exec(cmd, (error, stdout, stderr) => {
-         if (error) {
-            console.error("Failed to execute command, error: " + error);
-            return res.status(500).end();
-        }
+    let result = exec(cmd);
 
-        console.log(stdout);
-        console.log(stderr);
-        console.log("Repository " + req.params.name + " created!");
-        res.send(repo);
-    });
-});
+    console.log(result.stdout);
+    console.log(result.stderr);
+    console.log("Repository " + req.params.name + " created!");
+    this.type = "text/plain";
+    this.body = repo;
+}));
 
-let server = app.listen(config.port, () => {
-    console.log("GitStore listening at http://" + config.hostname + ":" + config.port);
-});
+app.listen(config.port);
+
+console.log("GitStore listening at http://" + config.hostname + ":" + config.port);
 
 // wget -O - -q http://localhost:3000/create/test3 | xargs git clone
