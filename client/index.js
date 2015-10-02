0cab78ce1134c2a1263e7f56bf0d7e01fe908c65
Mattias Runge <mattias.runge@ericsson.com>
2015-10-02 08:00:47 +0200

    Refactorization and start of nicer looking interfacee

---
+++ client/index.js
@@ -0,0 +1,27 @@
+"use strict";
+
+require.config({
+    baseUrl: ".",
+    paths: {
+        text: "node_modules/requirejs-text/text",
+        jquery: "node_modules/jquery/dist/jquery.min",
+        knockout: "node_modules/knockout/build/output/knockout-latest",
+        mdl: "node_modules/material-design-lite/material"
+    }
+});
+
+var components = [
+    "root-view"
+];
+
+require([
+    "knockout",
+    "lib/utils",
+    "lib/bindings"
+], function(ko, utils) {
+    utils.registerComponents(components);
+
+    var Model = function() {};
+
+    ko.applyBindings(new Model(), document.body);
+});
--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 "use strict";
 
-let fs = require("fs-promise");
-let exec = require("child-process-promise").exec;
 let koa = require("koa");
 let bodyParser = require("koa-bodyparser");
 let route = require("koa-route");
-let scriptDir = __dirname + "/scripts/";
+let staticFile = require("koa-static");
+let StatusError = require("./lib/status-error");
+let git = require("./lib/git");
 
 let config = require("/etc/gitstore.json");
-
-let KoaError = function(message, status) {
-    this.name = "KoaError";
-    this.message = message;
-    this.status = status || 500;
-    this.stack = (new Error()).stack;
-    this.toString = () => this.name + ": (status " + this.status + ") " + this.message;
-};
-KoaError.prototype = new Error();
-KoaError.prototype.name = "KoaError";
 
 let app = koa();
 app.name = "GitStore";
@@ -35,33 +25,26 @@ app.use(function*(next) {
     }
 });
 
+app.use(staticFile("./client"));
+
 app.use(route.get("/", function*() {
-    let items = yield fs.readdir(config.repoDir);
+    let items = yield git.list(config.hostname, config.repoDir);
     this.type = "text/plain";
-    this.body = items.map(item => "ssh://" + config.hostname + config.repoDir + "/" + item).join("\n");
+    this.body = items.join("\n");
+}));
+
+app.use(route.get("/list", function*() {
+    let items = yield git.list(config.hostname, config.repoDir);
+    this.type = "text/plain";
+    this.body = items.join("\n");
 }));
 
 app.use(route.post("/github", function*() {
     if (this.request.headers["x-github-event"] !== "push" & this.request.headers["x-github-event"] !== "ping") {
-        throw new KoaError("Got unknown GitHub request: " + this.request.headers["x-github-event"], 400);
+        throw new StatusError("Got unknown GitHub request: " + this.request.headers["x-github-event"], 400);
     }
 
-    console.log("Repository " + this.request.body.repository.name + " updated at GitHub!");
-
-    let exists = yield fs.exists(config.repoDir + "/" + this.request.body.repository.name);
-
-    if (!exists) {
-        throw new KoaError("Got GitHub push for repository we do not recognize, " + this.request.body.repository.name, 404);
-    }
-
-    let cmd = scriptDir + "update.sh " + config.repoDir + "/" + this.request.body.repository.name + " " + this.request.body.repository.ssh_url;
-
-    console.log("Executing " + cmd);
-    let result = yield exec(cmd);
-
-    console.log(result.stdout);
-    console.log(result.stderr);
-    console.log("Repository " + this.request.body.repository.name + " updated!");
+    yield git.updateFromGitHub(config.hostname, config.repoDir, this.request.body.repository.name, this.request.body.repository.ssh_url);
     this.type = "text/plain";
     this.body = "OK";
 }));
@@ -73,21 +56,7 @@ app.use(route.get("/update/:name", function*(name) {
 }));
 
 app.use(route.get("/create/:name", function*(name) {
-    let exists = yield fs.exists(config.repoDir + "/" + name);
-
-    if (exists) {
-        throw new KoaError("Repository already exists, " + name, 409);
-    }
-
-    let cmd = scriptDir + "create.sh " + scriptDir + " " + config.repoDir + " " + name + " http://" + config.hostname;
-    let repo = "ssh://" + config.hostname + config.repoDir + "/" + name;
-
-    console.log("Executing " + cmd);
-    let result = exec(cmd);
-
-    console.log(result.stdout);
-    console.log(result.stderr);
-    console.log("Repository " + name + " created!");
+    let repo = yield git.create(config.hostname, config.repoDir, name);
     this.type = "text/plain";
     this.body = repo;
 }));
