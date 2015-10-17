1c0548899c9e0b8e7d7ba75c892dfa2144a2f308
Mattias Runge <mattias@runge.se>
2015-10-17 15:29:12 +0200

    Improved the interface

--- client/components/list/model.js
+++ client/components/list/model.js
@@ -2,14 +2,25 @@
 
 define([
     "knockout",
-    "jquery"
-], function(ko, $) {
+    "mfw/socket"
+], function(ko, socket) {
     return function() {
         this.list = ko.observableArray();
 
         this.load = function() {
-            $.getJSON("list", function(data) {
-                this.list(data);
+            socket.emit("list", {}, function(error, list) {
+                if (error) {
+                    console.error(error);
+                    return;
+                }
+
+                list.sort(function(a, b) {
+                    return b.info.date.localeCompare(a.info.date);
+                });
+
+                console.log("list", list);
+
+                this.list(list);
             }.bind(this));
         }.bind(this);
 
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 "use strict";
 
 const mfw = require("mfw");
+const git = require("./lib/git");
 const config = require("/etc/gitstore.json");
 
-mfw({
-    name: "GitStore",
-    port: config.port,
-    api: require("./lib/http-api"),
-    routes: require("./lib/http-routes"),
-    client: __dirname + "/client"
-}).start();
+git.init(config.hostname, config.repoDir)
+.then(function() {
+    mfw({
+        name: "GitStore",
+        port: config.port,
+        api: require("./lib/http-api"),
+        routes: require("./lib/http-routes"),
+        client: __dirname + "/client"
+    }).start();
 
-console.log("GitStore listening at http://" + config.hostname + ":" + config.port);
+    console.log("GitStore listening at http://" + config.hostname + ":" + config.port);
+});
